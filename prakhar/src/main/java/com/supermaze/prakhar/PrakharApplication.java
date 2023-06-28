311cbc2f519f183321c75eb35aad240fc19a251a
Prakhar Tiwari <prakhar.tiwari@anduriltechnologies.com>
2023-06-28 15:17:15 +0530

    to check

--- prakhar/src/main/java/com/supermaze/prakhar/PrakharApplication.java
+++ prakhar/src/main/java/com/supermaze/prakhar/PrakharApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrakharApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrakharApplication.class, args);
+		SpringApplication.run(PrakharApplication.class, args); 
 	}
 
 }
